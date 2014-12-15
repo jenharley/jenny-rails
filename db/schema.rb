7cf0dbaa6b077ea8b8f7f4d3aabc33bedb515334
Steven Harley <smharley@gmail.com>
2014-12-15 14:55:43 -0800

    I hate auth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141107001039) do
+ActiveRecord::Schema.define(version: 20141215192853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,5 +59,18 @@ ActiveRecord::Schema.define(version: 20141107001039) do
 
   add_index "posters", ["name"], name: "index_posters_on_name", unique: true, using: :btree
   add_index "posters", ["slug"], name: "index_posters_on_slug", using: :btree
+
+  create_table "users", force: true do |t|
+    t.datetime "created_at",                                     null: false
+    t.datetime "updated_at",                                     null: false
+    t.boolean  "admin",                          default: false, null: false
+    t.string   "email",                                          null: false
+    t.string   "encrypted_password", limit: 128,                 null: false
+    t.string   "confirmation_token", limit: 128
+    t.string   "remember_token",     limit: 128,                 null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
