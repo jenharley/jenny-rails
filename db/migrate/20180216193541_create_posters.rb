21612d7bf25fabf49797d8d0ca7605de8020de10
Steven Harley <smharley@gmail.com>
2018-02-16 12:31:59 -0800

    Don't add category

--- db/migrate/20180216193541_create_posters.rb
+++ db/migrate/20180216193541_create_posters.rb
@@ -5,7 +5,6 @@ class CreatePosters < ActiveRecord::Migration[5.1]
       t.string :slug
       t.date :poster_date
       t.string :image_url
-      t.integer :category_id
 
       t.timestamps
     end
