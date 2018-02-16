cd8e456e9fa80e8986c062fa66f3306cf5ec4bbe
Steven Harley <smharley@gmail.com>
2018-02-16 12:54:47 -0800

    Add users

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!( email: "smharley@gmail.com", password: "password", admin: true )
+User.create!( email: "smharley@gmail.com", password: "password" )
 
 Poster.create!([
   {"id":54,"name":"The Understorey II",
