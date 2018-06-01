626105efdf55cb0bca6e2315a69ee1e8333bd54a
Steven Harley <smharley@gmail.com>
2018-06-01 15:56:36 -0700

    Update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,24 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_01_222625) do
+ActiveRecord::Schema.define(version: 2018_06_02_221357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "categories", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "categories_posters", id: :serial, force: :cascade do |t|
-    t.integer "category_id"
-    t.integer "poster_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.index ["category_id", "poster_id"], name: "index_categories_posters_on_category_id_and_poster_id"
-  end
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer "priority", default: 0, null: false
@@ -44,23 +30,42 @@ ActiveRecord::Schema.define(version: 2018_06_01_222625) do
     t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
-  create_table "posters", id: :serial, force: :cascade do |t|
-    t.string "name", limit: 255
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
     t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string "slug", limit: 255
-    t.date "poster_date"
-    t.string "image_url", limit: 255
-    t.integer "category_id"
-    t.index ["name"], name: "index_posters_on_name", unique: true
-    t.index ["slug"], name: "index_posters_on_slug"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id"
+    t.index ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type"
   end
 
-  create_table "users", id: :serial, force: :cascade do |t|
-    t.string "email", limit: 255, default: "", null: false
-    t.string "encrypted_password", limit: 255, default: "", null: false
+  create_table "lighthouses", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.float "latitude"
+    t.float "longitude"
+    t.string "description"
+    t.string "instagram"
+  end
+
+  create_table "posters", force: :cascade do |t|
+    t.string "name"
+    t.string "slug"
+    t.date "poster_date"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
     t.boolean "admin", default: false, null: false
-    t.string "reset_password_token", limit: 255
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
@@ -68,8 +73,8 @@ ActiveRecord::Schema.define(version: 2018_06_01_222625) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
