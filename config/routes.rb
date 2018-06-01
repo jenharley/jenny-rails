28791052098230c3632dfbe6959c908b9a3297ec
Steven Harley <smharley@gmail.com>
2018-06-01 15:41:02 -0700

    Add config

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,22 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+    resources :posters
+    resources :lighthouses
+
+    root to: "users#index"
+  end
+
+  devise_for :users
+  resources :posters
+  resources :lighthouses
+
+  root to: "posters#home"
+  get "contact" => "contact#new", :as => "contact"
+  post "contact" => "contact#create"
+
+  scope "api", defaults: {format: :json} do
+    resources :posters
+    resources :lighthouses
+  end
 end
