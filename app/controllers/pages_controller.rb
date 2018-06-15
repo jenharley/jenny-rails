856710e1d13654daa9409b691a6b245d55f53872
Steven Harley <smharley@gmail.com>
2018-06-15 16:30:13 -0700

    letsencrypt 3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
   def letsencrypt
-    render text: "PZcI-97YiVnvw9uTr_s4xTzuuJaQ0caqIVQaXztumx0.1eG2CMTjNPvpGKAvliDuItHt-gz7Xqq7XairJRxbv7w"
+    render text: "29hjAO9Vy_DxvoE81JjgiUz92HUPntRUWsGpRAest0o.1eG2CMTjNPvpGKAvliDuItHt-gz7Xqq7XairJRxbv7w"
   end
 end
